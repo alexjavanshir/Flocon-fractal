ed8b791b2bd3987470a62e3ae8399777274163b9
Alexandre Javanshir <61507640+alexjavanshir@users.noreply.github.com>
2022-03-24 17:17:44 +0000

    flocon recursif

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import*
+def trait(longueur_cote):
+    if longueur_cote<10:
+        forward(longueur_cote/3)
+        left(360/-6)
+        forward(longueur_cote/3)
+        left(360/3)
+        forward(longueur_cote/3)
+        left(360/-6)
+        forward(longueur_cote/3)
+    else:
+        trait(longueur_cote/3)
+        left(360/-6)
+        trait(longueur_cote/3)
+        left(360/3)
+        trait(longueur_cote/3)
+        left(360/-6)
+        trait(longueur_cote/3)
+
+def triangle():
+    l = 200
+    left(360/-6)
+    trait(l)
+    left(360/3)
+    trait(l)
+    left(360/3)
+    trait(l)
+
+
+triangle()
+done()
